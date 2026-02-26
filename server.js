07c46963d63b5a3181ee326e17163eb234727c91
xinyatang2022-boop <xinya.tang2022@gmail.com>
2026-02-26 10:57:07 -0500

    up server

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ let configDB = require('./config/db');
 let app = require("./config/express");
 let http = require('http');
 
+var server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 configDB().catch((err) => {
   console.error("DB connection failed:", err);
   process.exit(1);
 });
-var server = http.createServer(app);
 
-server.on('listening', onListening);
-const PORT = process.env.PORT || 3000;
-server.listen(PORT);
 
-function onListening() {
-  console.log(`Server running on port ${PORT}`);
-}
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+
+  
+  configDB()
+    .then(() => console.log("Backend successfully connected to MongoDB!"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+});
