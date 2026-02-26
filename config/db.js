aa10dc707773c4808b472645431ba90e3c984fc3
xinyatang2022-boop <xinya.tang2022@gmail.com>
2026-02-26 11:06:14 -0500

    edit db

--- config/db.js
+++ config/db.js
@@ -11,14 +11,10 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 
 module.exports = async function () {
   try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     await mongoose.connect(ConnectionString, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("==== Backend successfully connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
+    throw error; 
   }
 }
